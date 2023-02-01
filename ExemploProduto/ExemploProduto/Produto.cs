322e05b603e39406883d6f17ccdb6d25b338013d
Matheus Buchada <93539482+Buchadaz@users.noreply.github.com>
2023-01-31 22:10:48 -0300

    Add files via upload

---
+++ ExemploProduto/ExemploProduto/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploProduto
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public void RemoverProdutos(int retirar)
+        {
+            Quantidade -= retirar;
+        }
+        public void AdicionarProdutos(int quantidade) {
+            Quantidade += quantidade;
+        }
+        public double ValorTotalEstoque() {
+            return Preco * Quantidade;    
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidade(s), Total: $" + ValorTotalEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ ExemploProduto/ExemploProduto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExemploProduto;
+
+    class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p = new Produto();
+        
+        Console.WriteLine("Entre com os dados do produto: ");
+        Console.Write("Nome do produto: ");
+        p.Nome = Console.ReadLine();
+        Console.Write("Preço do produto: ");
+        p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Quantidade do produto: ");
+        p.Quantidade = int.Parse(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.Write("Os dados do produto são: " + p);
+        
+        Console.WriteLine();
+        Console.WriteLine("Digite o número de produtos a serem acrescentados: ");
+        Console.WriteLine();
+        int qte = int.Parse(Console.ReadLine());
+        p.AdicionarProdutos(qte);
+
+        Console.WriteLine();
+        Console.WriteLine("Dados atualizados: "+ p);
+        Console.WriteLine();
+        Console.WriteLine("Digite o número de produtos a serem retirados: ");
+        Console.WriteLine();
+        int ret = int.Parse(Console.ReadLine());
+        p.RemoverProdutos(ret);
+        Console.WriteLine("Dados atualizados: " + p);
+
+
+
+
+
+    }
+}
